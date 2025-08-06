a4ebcfe47250c6428199e62b887d36a45c1ea39b
Stephan Kulla <git.mail@kulla.me>
2025-08-06 19:05:21 +0200

    feat: Add sandbox example of lexical

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
 import './App.css'
 
+import { $getRoot, $getSelection } from 'lexical'
+import { useEffect } from 'react'
+
+import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin'
+import {
+  LexicalComposer,
+  type InitialConfigType,
+} from '@lexical/react/LexicalComposer'
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin'
+import { ContentEditable } from '@lexical/react/LexicalContentEditable'
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
+import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary'
+
+function Editor() {
+  const initialConfig: InitialConfigType = {
+    namespace: 'MyEditor',
+    onError(error) {
+      console.error('Editor error:', error)
+    },
+  }
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <RichTextPlugin
+        contentEditable={
+          <ContentEditable
+            aria-placeholder={'Enter some text...'}
+            placeholder={<div>Enter some text...</div>}
+          />
+        }
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <HistoryPlugin />
+      <AutoFocusPlugin />
+    </LexicalComposer>
+  )
+}
+
 export default function App() {
   return (
     <main className="prose p-10">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <h1>Lexical text editor:</h1>
+      <Editor />
     </main>
   )
 }
