cfc2381b5ca13383ab1fb4579a0a6d1dead8c847
Stephan Kulla <git.mail@kulla.me>
2025-08-09 22:30:41 +0200

    feat: Use TreeViewPanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css'
 
-import { useEffect, useState } from 'react'
+import {useEffect, useState} from 'react'
 
-import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin'
+import {AutoFocusPlugin} from '@lexical/react/LexicalAutoFocusPlugin'
 import {
   LexicalComposer,
   type InitialConfigType,
 } from '@lexical/react/LexicalComposer'
-import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin'
-import { ContentEditable } from '@lexical/react/LexicalContentEditable'
-import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
-import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary'
-import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
+import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin'
+import {ContentEditable} from '@lexical/react/LexicalContentEditable'
+import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin'
+import {LexicalErrorBoundary} from '@lexical/react/LexicalErrorBoundary'
+import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext'
 import ToolbarPlugin from './plugins/ToolbarPlugin'
 import {
   ExerciseNode,
@@ -19,10 +19,9 @@ import {
   SolutionNode,
   TaskNode,
 } from './plugins/exercise'
+import {TreeView} from '@lexical/react/LexicalTreeView'
 
 export default function App() {
-  const [editorState, setEditorState] = useState<unknown>(null)
-
   const initialConfig: InitialConfigType = {
     namespace: 'MyEditor',
     nodes: [ExerciseNode, SolutionNode, TaskNode],
@@ -34,8 +33,8 @@ export default function App() {
   return (
     <main className="prose p-10">
       <h1>Lexical text editor:</h1>
-      <section className="mb-4 relative border border-gray-300 rounded-lg p-4">
-        <LexicalComposer initialConfig={initialConfig}>
+      <LexicalComposer initialConfig={initialConfig}>
+        <div className="mb-4 relative border border-gray-300 rounded-lg p-4">
           <ToolbarPlugin />
           <RichTextPlugin
             contentEditable={
@@ -53,25 +52,15 @@ export default function App() {
           <HistoryPlugin />
           <AutoFocusPlugin />
           <ExerciseNodeTransformations />
-          <MyOnChangePlugin onChange={setEditorState} />
-        </LexicalComposer>
-      </section>
-      <h2>Editor state:</h2>
-      <pre className="p-4 rounded-lg">
-        {editorState ? JSON.stringify(editorState, null, 2) : 'No state yet'}
-      </pre>
+        </div>
+        <DebugPanel />
+      </LexicalComposer>
     </main>
   )
 }
 
-function MyOnChangePlugin({
-  onChange,
-}: { onChange: (editorState: unknown) => void }) {
+function DebugPanel() {
   const [editor] = useLexicalComposerContext()
-  useEffect(() => {
-    return editor.registerUpdateListener(({ editorState }) => {
-      onChange(editorState)
-    })
-  }, [editor, onChange])
-  return null
+
+  return <TreeView editor={editor} treeTypeButtonClassName='button' />
 }
